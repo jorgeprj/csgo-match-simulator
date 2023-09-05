7736983223e0d0e64be1c64e907ad10aa79c773d
jorgeprj <jlmp2002@gmail.com>
2023-09-05 18:08:56 -0300

    feat: update player profile

--- src/js/playerProfile.js
+++ src/js/playerProfile.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const responsePlayers = await fetch("/src/database/players.json");
     const players = await responsePlayers.json();
-    
+
+    const response = await fetch("/src/database/teams.json");
+    const teams = await response.json();
+
     function addClickEventToPlayerElements() {
         const playerElements = document.querySelectorAll(".player");
         playerElements.forEach(playerElement => {
@@ -9,26 +12,61 @@ document.addEventListener("DOMContentLoaded", async function () {
                 const stringPlayerId = this.getAttribute("data-player-id");
                 const playerId = parseInt(stringPlayerId, 10);
                 const player = players.find(player => player.id === playerId);
+
+                const flagImage = document.createElement("img");
+                flagImage.className = "flag";
+                const flagImagePath = `./assets/flags/${player.country}.png`;
+                flagImage.src = flagImagePath;
                 
-                document.getElementById("popupSkills").textContent = player.skills;
-                document.getElementById("popupName").textContent = player.name;
-                document.getElementById("popupNickname").textContent = player.nickname;
-                document.getElementById("popupAge").textContent = player.age;
-                document.getElementById("popupCountry").textContent = player.country;
+                document.getElementById("playerSkills").textContent = player.skills;
+                if (player.skills > 80){
+                    playerSkills.style.color = '#19a914';
+                }else if(player.skills < 80){
+                    playerSkills.style.color = '#49d184';
+                }
+
+                document.getElementById("playerNickname").textContent = player.nickname;
+                
+                const popupNameElement = document.getElementById("playerName");
+                popupNameElement.textContent = "";
+                popupNameElement.appendChild(flagImage);
+                popupNameElement.appendChild(document.createTextNode(player.name));
+
+                document.getElementById("playerAge").textContent = player.age;
+
+                const teamId = findTeamIdByPlayerId(playerId);
+                if (teamId !== null) {
+                    const team = teams.find(team => team.id === teamId);
+                    if (team !== undefined) {
+                        const logoImage = document.createElement("img");
+                        logoImage.className = "smallTeamLogo";
+                        const logoImagePath = `./assets/teams/${teamId}.png`;
+                        logoImage.src = logoImagePath;
+                        
+                        const popupTeamElement = document.getElementById("playerTeam");
+                        popupTeamElement.textContent = "";
+                        popupTeamElement.appendChild(logoImage);
+                        popupTeamElement.appendChild(document.createTextNode(team.name));
+                    }
+                }
 
                 const playerImage = document.getElementById("playerImage");
                 playerImage.src = `./assets/players/${player.id}.png`;
 
                 document.getElementById("playerPopupContainer").style.display = "block";
                 document.getElementById("playerPopup").style.display = "block";
-                if (player.skills > 80){
-                    popupSkills.style.color = '#19a914';
-                }else if(player.skills < 80){
-                    popupSkills.style.color = '#49d184';
-                }
-                    
+                
             });
         });
+    }
+
+    function findTeamIdByPlayerId(playerId) {
+        for (const team of teams) {
+            if (team.roster.includes(playerId)) {
+                return team.id;
+            }
+        }
+        return null;
     }
 
     playerPopupContainer.addEventListener("click", function (event) {
