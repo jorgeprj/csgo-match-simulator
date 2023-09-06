b8a7ab3011e70cbf9f8c8c454128328310019185
jorgeprj <jlmp2002@gmail.com>
2023-09-05 21:53:41 -0300

    feat: player id

--- src/js/playerProfile.js
+++ src/js/playerProfile.js
@@ -12,43 +12,12 @@ document.addEventListener("DOMContentLoaded", async function () {
                 const stringPlayerId = this.getAttribute("data-player-id");
                 const playerId = parseInt(stringPlayerId, 10);
                 const player = players.find(player => player.id === playerId);
-
-                const flagImage = document.createElement("img");
-                flagImage.className = "flag";
-                const flagImagePath = `./assets/flags/${player.country}.png`;
-                flagImage.src = flagImagePath;
-                
-                document.getElementById("playerSkills").textContent = player.skills;
-                if (player.skills > 80){
-                    playerSkills.style.color = '#19a914';
-                }else if(player.skills < 80){
-                    playerSkills.style.color = '#49d184';
-                }
-
-                document.getElementById("playerNickname").textContent = player.nickname;
-                
-                const popupNameElement = document.getElementById("playerName");
-                popupNameElement.textContent = "";
-                popupNameElement.appendChild(flagImage);
-                popupNameElement.appendChild(document.createTextNode(player.name));
-
+            
+                createPlayerSkills(player);
+                createPlayerNickname(player);
+                createPlayerName(player);
                 document.getElementById("playerAge").textContent = player.age;
-
-                const teamId = findTeamIdByPlayerId(playerId);
-                if (teamId !== null) {
-                    const team = teams.find(team => team.id === teamId);
-                    if (team !== undefined) {
-                        const logoImage = document.createElement("img");
-                        logoImage.className = "smallTeamLogo";
-                        const logoImagePath = `./assets/teams/${teamId}.png`;
-                        logoImage.src = logoImagePath;
-                        
-                        const popupTeamElement = document.getElementById("playerTeam");
-                        popupTeamElement.textContent = "";
-                        popupTeamElement.appendChild(logoImage);
-                        popupTeamElement.appendChild(document.createTextNode(team.name));
-                    }
-                }
+                createPlayerCurrentTeam(player);
 
                 const playerImage = document.getElementById("playerImage");
                 playerImage.src = `./assets/players/${player.id}.png`;
@@ -58,6 +27,67 @@ document.addEventListener("DOMContentLoaded", async function () {
                 
             });
         });
+    }
+
+    function createPlayerSkills(player){
+        document.getElementById("playerSkills").textContent = player.skills;
+        if (player.skills > 80){
+            playerSkills.style.color = '#19a914';
+        }else if(player.skills < 80){
+            playerSkills.style.color = '#49d184';
+        }
+    }
+
+    function createPlayerNickname(player){
+        const playerNicknameElement = document.getElementById("playerNickname");
+        playerNicknameElement.textContent = "";
+        const nicknameTextNode = document.createTextNode(player.nickname);
+        playerNicknameElement.appendChild(nicknameTextNode);
+        const playerIdElement = createPlayerId(player);
+        playerNicknameElement.appendChild(playerIdElement);
+    }
+
+    function createPlayerId(player){
+        const playerIdElement = document.createElement("span");
+        playerIdElement.classList.add("playerId");
+        const playerIdString = String(player.id);
+        const paddedPlayerId = playerIdString.padStart(4, '0');
+        playerIdElement.textContent = "#" + paddedPlayerId;
+        return playerIdElement;
+    }
+
+    function createFlagImage(player){
+        const flagImage = document.createElement("img");
+        flagImage.className = "flag";
+        const flagImagePath = `./assets/flags/${player.country}.png`;
+        flagImage.src = flagImagePath;
+        return flagImage;
+    }
+
+    function createPlayerName(player){
+        const flagImage = createFlagImage(player);
+        const playerNameElement = document.getElementById("playerName");
+        playerNameElement.textContent = "";
+        playerNameElement.appendChild(flagImage);
+        playerNameElement.appendChild(document.createTextNode(player.name));
+    }
+
+    function createPlayerCurrentTeam(player){
+        const teamId = findTeamIdByPlayerId(player.id);
+        if (teamId !== null) {
+            const team = teams.find(team => team.id === teamId);
+            if (team !== undefined) {
+                const logoImage = document.createElement("img");
+                logoImage.className = "smallTeamLogo";
+                const logoImagePath = `./assets/teams/${teamId}.png`;
+                logoImage.src = logoImagePath;
+                
+                const playerTeamElement = document.getElementById("playerTeam");
+                playerTeamElement.textContent = "";
+                playerTeamElement.appendChild(logoImage);
+                playerTeamElement.appendChild(document.createTextNode(team.name));
+            }
+        }
     }
 
     function findTeamIdByPlayerId(playerId) {
